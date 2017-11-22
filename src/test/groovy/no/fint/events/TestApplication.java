c19c12b1dd6d0cb7b25fccc6a11e2395aeff2350
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-11-22 22:30:02 +0100

    Spring mishap.

--- src/test/groovy/no/fint/events/TestApplication.java
+++ src/test/groovy/no/fint/events/TestApplication.java
@@ -1,8 +1,10 @@
 package no.fint.events;
 
+import no.fint.events.annotations.EnableFintEvents;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@EnableFintEvents
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
